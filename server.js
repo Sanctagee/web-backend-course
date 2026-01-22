25a7d91b90d56c78df0531b0b4db8a7cfe92960d
SanctaGee <tonygabito@gmail.com>
2026-01-22 11:49:54 +0100

    Modified to include the routing for: Utility and intentional trigger error

--- server.js
+++ server.js
@@ -12,13 +12,14 @@ const app = express()
 const static = require("./routes/static")
 const baseController = require("./controllers/baseController")
 const inventoryRoute = require("./routes/inventoryRoute")
+const utilities = require("./utilities/")  // ← ADDED THIS
 
 /* ***********************
  * View Engine and Templates
  *************************/
 app.set("view engine", "ejs")
 app.use(expressLayouts)
-app.set("layout", "./layouts/layout") // not at views root
+app.set("layout", "./layouts/layout")
 
 /* ***********************
  * Routes
@@ -32,7 +33,6 @@ app.use((req, res, next) => {
     : 'http://localhost:5500'
   next()
 })
-
 // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 /*
@@ -49,12 +49,45 @@ app.get("/inv/type/:classificationId", (req, res) => {
 // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 */
 
+
 // Index route
 app.get("/", baseController.buildHome)
 
 // Inventory routes
 app.use("/inv", inventoryRoute)
 
+// Intentional error route for testing
+app.get("/trigger-error", utilities.handleErrors(async (req, res, next) => {
+  throw new Error("Intentional 500 error for testing")
+}))
+
+/* ***********************
+ * File Not Found (404) Handler
+ * Must be after all other routes
+ *************************/
+app.use(async (req, res, next) => {
+  next({status: 404, message: 'Sorry, we appear to have lost that page.'})
+})
+
+/* ***********************
+ * Express Error Handler
+ * Place after all other middleware
+ *************************/
+app.use(async (err, req, res, next) => {
+  let nav = await utilities.getNav()
+  console.error(`Error at: "${req.originalUrl}": ${err.message}`)
+  let message
+  if(err.status == 404){ 
+    message = err.message
+  } else {
+    message = 'Oh no! There was a crash. Maybe try a different route?'
+  }
+  res.render("errors/error", {
+    title: err.status || 'Server Error',
+    message,
+    nav
+  })
+})
 
 /* ***********************
  * Local Server Information
