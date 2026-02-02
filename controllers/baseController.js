44de078c0d6216b9c6109507d5eb7bf28b37f051
SanctaGee <tonygabito@gmail.com>
2026-02-02 10:01:48 +0100

    added some lines of code when testing the flash message in different forms

--- controllers/baseController.js
+++ controllers/baseController.js
@@ -3,6 +3,22 @@ const baseController = {}
 
 baseController.buildHome = async function(req, res){
   const nav = await utilities.getNav()
+  /*===================USED THESE TO TEST THE FLASH MESSAGES ==============
+
+  // Test notice/info message
+  req.flash("notice", "This is a flash message.")
+
+  // Test warning message
+  req.flash("warning", "Please review your information carefully.")
+
+  // Test error message
+  req.flash("error", "Something went wrong. Please try again.")
+
+  // Test success message
+    req.flash("success", "Operation completed successfully!")
+
+   //=========================================================== */
+
   res.render("index", {title: "Home", nav})
 }
 
