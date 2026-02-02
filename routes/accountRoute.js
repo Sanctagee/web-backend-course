e9adeace1e495c40bd821b9d206e5c75b1c7c82c
SanctaGee <tonygabito@gmail.com>
2026-02-02 10:02:49 +0100

    created this route to handle the login and registration pages

---
+++ routes/accountRoute.js
@@ -0,0 +1,23 @@
+// Needed Resources 
+const express = require("express")
+const router = new express.Router() 
+const accountController = require("../controllers/accountController")
+const utilities = require("../utilities/")
+const regValidate = require("../utilities/account-validation")
+
+// Route to deliver login view
+router.get("/login", utilities.handleErrors(accountController.buildLogin))
+
+// Route to deliver registration view
+router.get("/register", utilities.handleErrors(accountController.buildRegister))
+
+// Process the registration data
+router.post(
+  "/register",
+  regValidate.registrationRules(),
+  regValidate.checkRegData,
+  utilities.handleErrors(accountController.registerAccount)
+)
+
+// Export the router
+module.exports = router;
