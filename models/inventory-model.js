fb243ea988cd0fff811e891d2a2f726f29a39d2f
SanctaGee <tonygabito@gmail.com>
2026-02-03 11:32:16 +0100

    updated to add the models that will add the classification table, that will check if the classification already exist, it will also check the inventory

--- models/inventory-model.js
+++ models/inventory-model.js
@@ -41,6 +41,75 @@ async function getInventoryByInventoryId(inv_id) {
   }
 }
 
+/* ***************************
+ *  To Add new classification
+ * ************************** */
+async function addClassification(classification_name) {
+  try {
+    const sql = "INSERT INTO classification (classification_name) VALUES ($1) RETURNING *";
+    return await pool.query(sql, [classification_name]);
+  } catch (error) {
+    console.error("addClassification error: " + error);
+    return null;
+  }
+}
 
+/* ***************************
+ *  Check if classification name already exists
+ * ************************** */
+async function checkExistingClassification(classification_name) {
+  try {
+    const sql = "SELECT * FROM classification WHERE classification_name = $1"
+    const result = await pool.query(sql, [classification_name])
+    return result.rowCount
+  } catch (error) {
+    return error.message
+  }
+}
 
-module.exports = {getClassifications, getInventoryByClassificationId, getInventoryByInventoryId}
+/* ***************************
+ *  Add new vehicle to inventory
+ * ************************** */
+async function addInventory(
+  inv_make, 
+  inv_model, 
+  inv_year, 
+  inv_description, 
+  inv_image, 
+  inv_thumbnail, 
+  inv_price, 
+  inv_miles, 
+  inv_color, 
+  classification_id
+) {
+  try {
+    const sql = `INSERT INTO inventory 
+      (inv_make, inv_model, inv_year, inv_description, inv_image, inv_thumbnail, inv_price, inv_miles, inv_color, classification_id) 
+      VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10) RETURNING *`
+    
+    return await pool.query(sql, [
+      inv_make, 
+      inv_model, 
+      inv_year, 
+      inv_description, 
+      inv_image, 
+      inv_thumbnail, 
+      inv_price, 
+      inv_miles, 
+      inv_color, 
+      classification_id
+    ])
+  } catch (error) {
+    return error.message
+  }
+}
+
+// Updated module.exports to include the new function
+module.exports = {
+  getClassifications, 
+  getInventoryByClassificationId, 
+  getInventoryByInventoryId,
+  addClassification,
+  checkExistingClassification,
+  addInventory
+}
