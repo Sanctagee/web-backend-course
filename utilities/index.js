c610ac80cbdeccf811f8c83947d288d9b82c49ef
SanctaGee <tonygabito@gmail.com>
2026-02-12 11:57:30 +0100

    modified this file to include the middleware that checks for validity, as well as the jwtoken

--- utilities/index.js
+++ utilities/index.js
@@ -1,5 +1,7 @@
 const invModel = require("../models/inventory-model")
 const Util = {}
+const jwt = require("jsonwebtoken")
+require("dotenv").config()
 
 /* ************************
  * To create the nav HTML unordered list
@@ -128,10 +130,56 @@ Util.buildClassificationList = async function (classification_id = null) {
  **************************************** */
 Util.handleErrors = fn => (req, res, next) => Promise.resolve(fn(req, res, next)).catch(next)
 
-module.exports = Util
 
+/* ****************************************
+* Middleware to check token validity
+**************************************** */
+Util.checkJWTToken = (req, res, next) => {
+ if (req.cookies.jwt) {
+  jwt.verify(
+   req.cookies.jwt,
+   process.env.ACCESS_TOKEN_SECRET,
+   function (err, accountData) {
+    if (err) {
+     req.flash("notice", "Please log in")
+     res.clearCookie("jwt")
+     return res.redirect("/account/login")
+    }
+    res.locals.accountData = accountData
+    res.locals.loggedin = 1
+    next()
+   })
+ } else {
+  next()
+ }
+}
 
+/* ****************************************
+ *  Check Login
+ *  JWT authorize activity
+ * ************************************ */
+Util.checkLogin = (req, res, next) => {
+  if (res.locals.loggedin) {
+    next()
+  } else {
+    req.flash("notice", "Please log in.")
+    return res.redirect("/account/login")
+  }
+}
 
-
+/* ****************************************
+ *  Check Account Type (Employee or Admin)
+ *  Assignment 5, Task 2
+ * ************************************ */
+Util.checkAccountType = (req, res, next) => {
+  if (res.locals.loggedin && res.locals.accountData && 
+      (res.locals.accountData.account_type === "Employee" || 
+       res.locals.accountData.account_type === "Admin")) {
+    next()
+  } else {
+    req.flash("notice", "Please log in with an Employee or Admin account.")
+    return res.redirect("/account/login")
+  }
+}
 
 module.exports = Util
