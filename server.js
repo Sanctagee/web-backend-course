5b6d96c60bb986b89db377a360abf99f48ff5778
SanctaGee <tonygabito@gmail.com>
2026-02-12 11:32:23 +0100

    added the utility link that handles the JWTToken

--- server.js
+++ server.js
@@ -61,7 +61,7 @@ app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-
 app.use(cookieParser())
 
 // Login Process Activity
-// app.use(utilities.checkJWTToken)
+app.use(utilities.checkJWTToken)
 
 
 
