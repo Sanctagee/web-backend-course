42f925788275476dc976fed4c592bcfdb795db97
SanctaGee <tonygabito@gmail.com>
2026-02-12 11:42:02 +0100

    added the routes that: - updates the account -updates the password -checksLogin

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -11,8 +11,14 @@ router.get("/login", utilities.handleErrors(accountController.buildLogin))
 // Route to deliver registration view
 router.get("/register", utilities.handleErrors(accountController.buildRegister))
 
+// Logout route
+router.get("/logout", utilities.handleErrors(accountController.logout))
+
 // Route to deliver account management view
-router.get("/", utilities.handleErrors(accountController.buildAccountManagement))
+router.get(
+  "/", 
+  utilities.checkLogin,
+  utilities.handleErrors(accountController.buildAccountManagement))
 
 // Process the Login Activity
 router.post(
@@ -28,6 +34,29 @@ router.post(
   regValidate.registrationRules(),
   regValidate.checkRegData,
   utilities.handleErrors(accountController.registerAccount)
+)
+
+// Route to deliver account update view
+router.get(
+  "/update",
+  utilities.checkLogin,
+  utilities.handleErrors(accountController.buildAccountUpdate)
+)
+
+// Process account info update
+router.post(
+  "/update-account",
+  regValidate.accountUpdateRules(),
+  regValidate.checkAccountUpdate,
+  utilities.handleErrors(accountController.updateAccount)
+)
+
+// Process password update
+router.post(
+  "/update-password",
+  regValidate.passwordUpdateRules(),
+  regValidate.checkPasswordUpdate,
+  utilities.handleErrors(accountController.updatePassword)
 )
 
 // Export the router
